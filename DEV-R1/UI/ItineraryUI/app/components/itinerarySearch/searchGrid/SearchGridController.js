e18c57125877bd05bc020f68bc349a017788ff24
sandesh89 <sandesh89@gmail.com>
2015-01-26 20:35:28 +0530

    Changes to search grid

--- DEV-R1/UI/ItineraryUI/app/components/itinerarySearch/searchGrid/SearchGridController.js
+++ DEV-R1/UI/ItineraryUI/app/components/itinerarySearch/searchGrid/SearchGridController.js
@@ -2,29 +2,33 @@
 	var app = angular.module("itinerary");
 
 	var SearchGridController = function($scope) {
-		$scope.oneAtATime = true;
-
-		$scope.days = [ {
-			title : 'Day 1',
-			contents : [ 'Travel', 'Sightseeing', 'Hotel' ]
+		$scope.itineraries = [ {
+			title : 'Trip to Himachal Pradesh',
+			destinations : [ 'Shimla', 'Manali', 'Dalhousie' ],
+			imgSrc:'http://upload.wikimedia.org/wikipedia/commons/2/20/Khajjiar.jpg',
+			startDate:'01/26/2015',
+			endDate:'01/30/2015',
+			modesOfTransport:['Flight, Train, Car'],
+			cost:'Rs 30,000'
 		}, {
-			title : 'Day 2',
-			contents : [ 'Travel', 'Sightseeing', 'Hotel' ]
-		} ];
+			title : 'Trip to Andaman and Nicobar Islands',
+			destinations : [ 'Havelock Island', 'Smith Island', 'Anderson Island' ],
+			imgSrc:'http://upload.wikimedia.org/wikipedia/commons/c/c2/Wandoor_Beach_Andaman_4160169.JPG',
+			startDate:'10/26/2014',
+			endDate:'11/3/2014',
+			modesOfTransport:['Flight, Ship'],
+			cost:'Rs 70,000'
+		}, {
+			title : 'Trip to Gujarat',
+			destinations : [ 'Ahmedabad', 'Gandhinagar', 'Surat' ],
+			imgSrc:'http://upload.wikimedia.org/wikipedia/commons/8/8b/Ahmedabad_BRTS.jpg',
+			startDate:'02/26/2015',
+			endDate:'02/30/2015',
+			modesOfTransport:['Flight, Train, Car'],
+			cost:'Rs 30,000'
+		}
+		];
 
-		$scope.addDay = function() {
-			var newItemNo = $scope.days.length + 1;
-			var newDay = {
-				title : 'Day ' + newItemNo,
-				contents : [ 'Travel', 'Sightseeing', 'Hotel' ]
-			};
-			$scope.days.push(newDay);
-		};
-
-		$scope.removeLastDay = function() {
-			$scope.days.pop();
-		};
-		
 	};
 
 	app.controller("SearchGridController", SearchGridController);
